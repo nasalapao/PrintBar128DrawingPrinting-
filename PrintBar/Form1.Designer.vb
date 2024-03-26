ca5f837481624a358e593e4ee6c6e6425ce88d5b
nasalapao <nasalapao@gmail.com>
2024-03-26 15:34:25 +0700

    Add project files.

---
+++ PrintBar/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.bntPrint = New System.Windows.Forms.Button()
+        Me.PD = New System.Drawing.Printing.PrintDocument()
+        Me.PPD = New System.Windows.Forms.PrintPreviewDialog()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'bntPrint
+        '
+        Me.bntPrint.Location = New System.Drawing.Point(290, 333)
+        Me.bntPrint.Name = "bntPrint"
+        Me.bntPrint.Size = New System.Drawing.Size(75, 23)
+        Me.bntPrint.TabIndex = 0
+        Me.bntPrint.Text = "Button1"
+        Me.bntPrint.UseVisualStyleBackColor = True
+        '
+        'PD
+        '
+        '
+        'PPD
+        '
+        Me.PPD.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PPD.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PPD.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PPD.Enabled = True
+        Me.PPD.Icon = CType(resources.GetObject("PPD.Icon"), System.Drawing.Icon)
+        Me.PPD.Name = "PrintPreviewDialog1"
+        Me.PPD.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(195, 37)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(308, 237)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.bntPrint)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents bntPrint As Button
+    Friend WithEvents PD As Printing.PrintDocument
+    Friend WithEvents PPD As PrintPreviewDialog
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ PrintBar/Form1.vb
@@ -0,0 +1,89 @@
+﻿Imports System.Drawing.Printing
+Imports PrintBar.My.Resources
+Imports ZXing
+Public Class Form1
+
+
+
+    Dim longpaper As Integer
+    Sub changelongpaper()
+        Dim rowcount As Integer
+        longpaper = 0
+        ' rowcount = DataGridView1.Rows.Count
+        longpaper = rowcount * 15
+        longpaper = longpaper + 240
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim barcodewritte As New BarcodeWriter
+        barcodewritte.Format = BarcodeFormat.CODE_128
+
+        Dim img = barcodewritte.Write("123456000DDDSS")
+
+        PictureBox1.Image = img
+
+    End Sub
+
+    Private Sub bntPrint_Click(sender As Object, e As EventArgs) Handles bntPrint.Click
+        changelongpaper()
+        PPD.Document = PD
+        PPD.ShowDialog()
+        'PD.Print()  'Direct Print
+    End Sub
+
+    Private Sub PD_BeginPrint(sender As Object, e As PrintEventArgs) Handles PD.BeginPrint
+        Dim pagesetup As New PageSettings
+        pagesetup.PaperSize = New PaperSize("Custom", 314, 196) 'fixed size
+
+        'pagesetup.PaperSize = New PaperSize("Custom", 250, longpaper)
+        PD.DefaultPageSettings = pagesetup
+    End Sub
+
+    Private Sub PD_PrintPage(sender As Object, e As PrintPageEventArgs) Handles PD.PrintPage
+        Dim f16b As New Font("TH Niramit AS", 16, FontStyle.Bold)
+        Dim f12 As New Font("TH Niramit AS", 12, FontStyle.Regular)
+        Dim f10b As New Font("TH Niramit AS", 10, FontStyle.Bold)
+        Dim f10 As New Font("TH Niramit AS", 10, FontStyle.Regular)
+        Dim f8b As New Font("TH Niramit AS", 8, FontStyle.Bold)
+        Dim f8 As New Font("TH Niramit AS", 8, FontStyle.Regular)
+
+        Dim marginL As Integer = PD.DefaultPageSettings.Margins.Left
+        Dim marginC As Integer = PD.DefaultPageSettings.PaperSize.Width / 2
+        Dim marginR As Integer = PD.DefaultPageSettings.PaperSize.Width
+
+        Dim Right As New StringFormat
+        Dim Center As New StringFormat
+        Right.Alignment = StringAlignment.Far
+        Center.Alignment = StringAlignment.Center
+
+        Dim Line As String = "--------------------------------------------"
+
+        Dim logoImage As Image = Resource1.ResourceManager.GetObject("logo2")
+        e.Graphics.DrawImage(logoImage, CInt((e.PageBounds.Width - 150) / 2), 5, 150, 35)
+        'e.Graphics.DrawImage(logoImage, 0, 250, 150, 50)
+
+        e.Graphics.DrawString("RD Label Scale", f16b, Brushes.Black, marginC, 8, Center)
+        e.Graphics.DrawString("RD Room Location : 1", f10, Brushes.Black, marginC, 35, Center)
+        e.Graphics.DrawString("Non Allergen", f10, Brushes.Black, marginC, 50, Center)
+        e.Graphics.DrawString("Smile Heart Foods Co.,Ltd.", f10b, Brushes.Black, marginC, 70, Center)
+        e.Graphics.DrawString(Line, f10, Brushes.Black, marginC, 85, Center)
+
+        Dim adjustMargin As Integer = 18
+        e.Graphics.DrawString("ประเภท", f10b, Brushes.Black, 8, 100)
+        e.Graphics.DrawString("DMEMO", f10, Brushes.Black, marginR - adjustMargin, 100, Right)
+
+        e.Graphics.DrawString("ชื่อพนักงาน", f10b, Brushes.Black, 8, 115)
+        e.Graphics.DrawString("DEMO", f10, Brushes.Black, marginR - adjustMargin, 115, Right)
+
+        e.Graphics.DrawString("เวลาที่ชั่ง", f10b, Brushes.Black, 8, 130)
+        e.Graphics.DrawString("13/08/2024 11:11", f10, Brushes.Black, marginR - adjustMargin, 130, Right)
+
+        e.Graphics.DrawString("เวลาที่ปริ้น", f10b, Brushes.Black, 8, 145)
+        e.Graphics.DrawString("13/08/2024 13:13", f10, Brushes.Black, marginR - adjustMargin, 145, Right)
+
+
+        e.Graphics.DrawImage(PictureBox1.Image, CInt((e.PageBounds.Width - 300) / 2), 150, 300, 40)
+
+
+
+    End Sub
+End Class
